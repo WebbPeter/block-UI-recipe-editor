011621664e6f96a33d9ba7115ccc5b3c99f2d3c3
WebbPeter <Webb.Peter.A@gmail.com>
2024-07-09 13:55:51 -0500

    updating theme / branding of UI
    modify the theme of the ui - including the chart to feel more like marsfarm branding - cool and space themed for high school kids. kid of mission controlley

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ document.getElementById('output').addEventListener('input', function(event) {
     if (event.target.classList.contains('start-time')) {
       jsonData.phases[phaseIndex].step[stepIndex][key][settingIndex].start_time = event.target.value.split(',').map(Number);
     } else if (event.target.classList.contains('setting')) {
-      jsonData.phases[phaseIndex].step[stepIndex][key][settingIndex].setting = Array.isArray(jsonData.phases[phaseIndex].step[stepIndex][key][settingIndex].setting) ? event.target.value.split(',').map(Number) : Number(event.target.value);
+      jsonData.phases[phaseIndex].step[stepIndex][key][settingIndex].setting = Array.isArray(jsonData.phases[phaseIndex].step[stepIndex][key][settingIndex].setting) ? event.target.value.split(', ').map(Number) : Number(event.target.value);
     }
 
     document.getElementById('jsonInput').value = JSON.stringify(jsonData, null, 2);
@@ -206,3 +206,189 @@ function getColorForKey(key) {
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// Blockly JSON functions for converting JSON to workspace and workspace to JSON
+
+Blockly.JSON = new Object();
+
+Blockly.JSON.fromWorkspace = function(workspace) {
+  var blocks = workspace.getTopBlocks(true);
+  var json = Blockly.JSON.generalBlockToObj(blocks[0]);
+  return JSON.stringify(json, null, 2);
+};
+
+Blockly.JSON.generalBlockToObj = function(block) {
+  var obj = null;
+  switch (block.type) {
+    case 'dictionary':
+      obj = {};
+      var input = block.getInput('ADD');
+      var connections = input.connection.targetConnection;
+      while (connections) {
+        var pairBlock = connections.getSourceBlock();
+        var key = pairBlock.getFieldValue('key');
+        obj[key] = Blockly.JSON.generalBlockToObj(pairBlock.getInputTargetBlock('value'));
+        connections = pairBlock.nextConnection.targetConnection;
+      }
+      break;
+    case 'array':
+      obj = [];
+      var input = block.getInput('ADD');
+      var connections = input.connection.targetConnection;
+      while (connections) {
+        var valueBlock = connections.getSourceBlock();
+        obj.push(Blockly.JSON.generalBlockToObj(valueBlock));
+        connections = valueBlock.nextConnection.targetConnection;
+      }
+      break;
+    case 'number':
+      obj = Number(block.getFieldValue('number'));
+      break;
+    case 'string':
+      obj = block.getFieldValue('text');
+      break;
+    case 'true':
+      obj = true;
+      break;
+    case 'false':
+      obj = false;
+      break;
+  }
+  return obj;
+};
+
+Blockly.JSON.toWorkspace = function(jsonText, workspace) {
+  var json = JSON.parse(jsonText);
+  var rootBlock = Blockly.JSON.buildAndConnect(json, workspace, null);
+  if (rootBlock) {
+    rootBlock.render();
+  }
+};
+
+Blockly.JSON.buildAndConnect = function(obj, workspace, parentBlock) {
+  var newBlock = null;
+  if (Array.isArray(obj)) {
+    newBlock = workspace.newBlock('array');
+    for (var i = 0; i < obj.length; i++) {
+      var itemBlock = Blockly.JSON.buildAndConnect(obj[i], workspace, newBlock);
+      newBlock.getInput('ADD').connection.connect(itemBlock.previousConnection);
+    }
+  } else if (typeof obj === 'object') {
+    newBlock = workspace.newBlock('dictionary');
+    for (var key in obj) {
+      var pairBlock = workspace.newBlock('keyvaluepair');
+      pairBlock.setFieldValue(key, 'key');
+      var valueBlock = Blockly.JSON.buildAndConnect(obj[key], workspace, pairBlock);
+      pairBlock.getInput('value').connection.connect(valueBlock.outputConnection);
+      newBlock.getInput('ADD').connection.connect(pairBlock.previousConnection);
+    }
+  } else if (typeof obj === 'number') {
+    newBlock = workspace.newBlock('number');
+    newBlock.setFieldValue(obj, 'number');
+  } else if (typeof obj === 'string') {
+    newBlock = workspace.newBlock('string');
+    newBlock.setFieldValue(obj, 'text');
+  } else if (typeof obj === 'boolean') {
+    newBlock = workspace.newBlock(obj ? 'true' : 'false');
+  }
+  if (parentBlock) {
+    parentBlock.nextConnection.connect(newBlock.previousConnection);
+  }
+  return newBlock;
+};
+
+// Custom Blockly blocks definitions
+
+Blockly.Blocks['dictionary'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Dictionary");
+    this.appendStatementInput("ADD")
+        .setCheck("KeyValuePair");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['array'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Array");
+    this.appendStatementInput("ADD")
+        .setCheck("Value");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['keyvaluepair'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Key")
+        .appendField(new Blockly.FieldTextInput("key"), "key");
+    this.appendValueInput("value")
+        .setCheck("Value")
+        .appendField("Value");
+    this.setPreviousStatement(true, "KeyValuePair");
+    this.setNextStatement(true, "KeyValuePair");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['number'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Number")
+        .appendField(new Blockly.FieldNumber(0), "number");
+    this.setOutput(true, "Value");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['string'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("String")
+        .appendField(new Blockly.FieldTextInput("text"), "text");
+    this.setOutput(true, "Value");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['true'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("True");
+    this.setOutput(true, "Value");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['false'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("False");
+    this.setOutput(true, "Value");
+    this.setColour(230);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
+// Extending Blockly functionalities
+
+Blockly.Blocks['dictionary'].appendValueInput = function(name) {
+  var input = new Blockly.Input(Blockly.DUMMY_INPUT, name, this, 0);
+  this.inputList.push(input);
+  return input;
+};
